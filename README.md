51428ad4b995000d40b00211cc428d703411196e
libin ann babu <libin.babu@altd.in>
2023-10-31 18:16:17 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+https://www.youtube.com/watch?v=MWSP6PGHAb4
+
+npm create vue@latest
+
+npm install vuetify
+
+npm install @mdi/font -D
+
+https://vuetifyjs.com/en/getting-started/installation/
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+// Vuetify
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  defaults:{
+    VBtn: { variant: "outlined" },
+    VTextField: { variant: "solo" },
+  },
+  theme: {
+    defaultTheme: 'dark'
+  }
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.use(vuetify).mount('#app')
